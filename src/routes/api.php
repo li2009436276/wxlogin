73c168bbd882a2b1080a02e55bf749de5b804a76
li2009436276 <571816409@qq.com>
2023-05-24 16:03:31 +0800

    用户登录

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,11 @@
 Route::post('/applet/auth','AppletController@auth');
 Route::post('/applet/phone','AppletController@phone');
 Route::post('/applet/authAndPhone','AppletController@authAndPhone');
-Route::post('/applet/bindPhone','AppletController@bindPhone');
 Route::post('/applet/authBindUser','AppletController@authBindUser');
 Route::post('/applet/login','AppletController@login');
+
+
+Route::middleware('user.auth')->group(function (){
+
+    Route::post('/applet/bindPhone','AppletController@bindPhone');
+});
