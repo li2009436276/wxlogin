6a69a762d1064d4c0e35242c6bf0c434823f7e0a
571816409@qq.com <571816409@qq.com>
2023-05-27 16:55:21 +0800

    config找不到的问题

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,3 +6,6 @@ Route::post('/applet/phone','AppletController@phone');
 Route::post('/applet/authAndPhone','AppletController@authAndPhone');
 Route::post('/applet/authBindUser','AppletController@authBindUser');
 Route::post('/applet/login','AppletController@login');
+
+//公众号
+Route::post('/official/auth','OfficialController@auth');
